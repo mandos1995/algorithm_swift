fd69b616ffe3ec32574c328cef80632c22e7552e
mandos1995 <mandos19950620@gmail.com>
2021-06-11 13:30:09 +0900

    BOJ 9498 sol

---
+++ BOJ/9498_시험 성적.swift
@@ -0,0 +1,17 @@
+import Foundation
+let score = Int(readLine()!)!
+
+switch score/10 {
+case 10:
+    print("A")
+case 9:
+    print("A")
+case 8:
+    print("B")
+case 7:
+    print("C")
+case 6:
+    print("D")
+default:
+    print("F")
+}
