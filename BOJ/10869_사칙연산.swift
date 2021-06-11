0eff3e2b47c2df582cc7c3ae5f22db3999f0643d
mandos1995 <mandos19950620@gmail.com>
2021-06-11 12:43:30 +0900

    BOJ 10869 sol

---
+++ BOJ/10869_사칙연산.swift
@@ -0,0 +1,10 @@
+import Foundation
+let line = readLine()!
+let lineArr = line.components(separatedBy: " ")
+var a = Int(lineArr[0])!
+var b = Int(lineArr[1])!
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
