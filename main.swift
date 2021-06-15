f723b5b434c5818bb2bc68aca6b44039a537e7ea
minseok kim <77199797+mandos1995@users.noreply.github.com>
2021-06-15 15:52:22 +0900

    Delete main.swift

--- main.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-let num = readLine()!.components(separatedBy: " ").map{ Int($0)! }
-for _ in 0..<num[0]{
-    let input = readLine()!.components(separatedBy: " ").map{ Int($0)! }
-}
-print("비와이")
