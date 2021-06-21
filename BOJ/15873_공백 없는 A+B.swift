abb75b1f2db6e844e9a8098d4641dad618adc879
mandos1995 <mandos19950620@gmail.com>
2021-06-21 23:42:16 +0900

    BOJ 15921 sol

---
+++ BOJ/15873_공백 없는 A+B.swift
@@ -0,0 +1,11 @@
+import Foundation
+let input = readLine()!.compactMap { Int(String($0))}
+if input.count == 3 && input[0] == 1 && input[1] == 0{
+    print(10 + input[2])
+} else if input.count == 3 && input[1] == 1 {
+    print(input[0] + 10)
+} else if input.count == 4{
+    print(20)
+} else{
+    print(input[0] + input[1])
+}
