7dfa558486fa6e1bc5e4bc57220377c6d93eae1d
mandos1995 <mandos19950620@gmail.com>
2021-06-24 22:38:59 +0900

    BOJ 17388 sol

--- main.swift
+++ main.swift
@@ -1,10 +1,18 @@
 import Foundation
 let input = readLine()!.split(separator: " ").map { Int($0)! }
-var count = 0
-let max = input.max()!
-for i in input{
-    if max > i {
-        count = count + (max - i)
+if input[0] + input[1] + input[2] > 99 {
+    print("OK")
+} else {
+    let min = input.min()!
+    switch input.firstIndex(of: min) {
+    case 0:
+        print("Soongsil")
+    case 1:
+        print("Korea")
+    case 2:
+        print("Hanyang")
+    default:
+        print("잘못된 입력")
     }
 }
-print(count)
+
