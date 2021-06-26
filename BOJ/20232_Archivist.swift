860933d73469fcc8b280ed23a0c71b91c7e59d5e
mandos1995 <mandos19950620@gmail.com>
2021-06-26 15:01:10 +0900

    BOJ 20232 sol

---
+++ BOJ/20232_Archivist.swift
@@ -0,0 +1,3 @@
+var player = ["ITMO","SPbSU","SPbSU","ITMO","ITMO","SPbSU","ITMO","ITMO","ITMO","ITMO","ITMO","PetrSU, ITMO","SPbSU","SPbSU","ITMO","ITMO","ITMO","ITMO","SPbSU","ITMO","ITMO","ITMO","ITMO","SPbSU","ITMO"]
+let input = Int(readLine()!)!
+print(player[input - 1995])
