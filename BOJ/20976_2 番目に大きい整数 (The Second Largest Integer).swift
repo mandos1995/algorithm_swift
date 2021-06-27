dad9309bd99f4d0110b43bfe66a70b25dabb4d0a
mandos1995 <mandos19950620@gmail.com>
2021-06-27 15:49:16 +0900

    BOJ 20976 sol

---
+++ BOJ/20976_2 番目に大きい整数 (The Second Largest Integer).swift
@@ -0,0 +1,4 @@
+var input = readLine()!.split(separator: " ").map { Int($0)! }
+input.remove(at: input.firstIndex(of: input.min()!)!)
+input.remove(at: input.firstIndex(of: input.max()!)!)
+print(input[0])
