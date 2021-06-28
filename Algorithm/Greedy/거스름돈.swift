5c5d994295a95c2404b47ef9957608f8b6919589
mandos1995 <mandos19950620@gmail.com>
2021-06-28 14:00:33 +0900

    거스름돈.swift update

---
+++ Algorithm/Greedy/거스름돈.swift
@@ -0,0 +1,12 @@
+var n = 1260
+var count = 0
+
+// 큰 단위의 화폐부터 차례대로 확인
+var coin_types = [500, 100, 50, 10]
+
+for coin in coin_types{
+    count += n / coin // 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
+    n %= coin
+}
+
+print(count)
