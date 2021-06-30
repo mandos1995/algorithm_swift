8b3fe053e2e8ca91ddd7aa3d17926787f26a09c2
mandos1995 <mandos19950620@gmail.com>
2021-06-30 23:44:44 +0900

    BOJ 2445 sol

---
+++ BOJ/2445_별 찍기 - 8.swift
@@ -0,0 +1,26 @@
+let n = Int(readLine()!)!
+
+for i in 0..<n{
+    for _ in 0...i{
+        print("*",terminator: "")
+    }
+    for _ in i..<2 * (n - 1) - i{
+        print(" ",terminator: "")
+    }
+    for _ in 0...i{
+        print("*",terminator: "")
+    }
+    print()
+}
+for i in 0..<n-1{
+    for _ in i..<n-1{
+        print("*",terminator: "")
+    }
+    for _ in 0..<2 * i + 2{
+        print(" ",terminator: "")
+    }
+    for _ in i..<n-1{
+        print("*",terminator: "")
+    }
+    print()
+}
