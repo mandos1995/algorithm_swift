cd4d307cbd0709fbd781d9db3dee766e143c0c6b
mandos1995 <mandos19950620@gmail.com>
2021-07-02 15:19:36 +0900

    BOJ 2490 sol

--- BOJ/2490_윷놀이.swift
+++ BOJ/2490_윷놀이.swift
@@ -1,8 +1,24 @@
-//
-//  2490_윷놀이.swift
-//  algorithm_swift
-//
-//  Created by 김민석 on 2021/07/02.
-//
-
-import Foundation
+for _ in 0..<3 {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    var sum = 0
+    
+    for i in 0..<input.count {
+        if input[i] == 1 {
+            sum += 1
+        }
+    }
+    switch sum {
+    case 0:
+        print("D")
+    case 1:
+        print("C")
+    case 2:
+        print("B")
+    case 3:
+        print("A")
+    case 4:
+        print("E")
+    default:
+        print("없는 결과")
+    }
+}
