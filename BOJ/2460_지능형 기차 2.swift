2e1e2a30e591df8499aba8cbdb840f03bdd686d1
mandos1995 <mandos19950620@gmail.com>
2021-07-02 14:23:18 +0900

    BOJ 2460 sol

---
+++ BOJ/2460_지능형 기차 2.swift
@@ -0,0 +1,12 @@
+var people = 0
+var peopleSave = [0]
+for i in 0..<10{
+    people = peopleSave[i]
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let trainOff = input[0]
+    let trainOn = input[1]
+    people = people - trainOff
+    people = people + trainOn
+    peopleSave.append(people)
+}
+print(peopleSave.max()!)
