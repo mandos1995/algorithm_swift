a573aa8999dcb0e87bb8cac7951ae7654e5a550c
mandos1995 <mandos19950620@gmail.com>
2021-07-04 16:05:40 +0900

    상하좌우 update

---
+++ Algorithm/구현/상하좌우.swift
@@ -0,0 +1,30 @@
+// N을 입력받기
+let n = Int(readLine()!)!
+var x = 1
+var y = 1
+let plans = readLine()!.split(separator: " ")
+
+// L, R, U, D에 따른 이동 방향
+let dx = [0, 0, -1, 1]
+let dy = [-1, 1, 0, 0]
+let move_typse = ["L", "R", "U", "D"]
+
+var nx = 0
+var ny = 0
+// 이동 계확을 하나씩 확인
+for plan in plans {
+    // 이동 후 좌표 구하기
+    for i in 0..<move_typse.count {
+        if plan == move_typse[i] {
+            nx = x + dx[i]
+            ny = y + dy[i]
+        }
+    }
+    if nx < 1 || ny < 1 || nx > n || ny > n{
+        continue
+    }
+    
+    x = nx
+    y = ny
+}
+print(x, y)
