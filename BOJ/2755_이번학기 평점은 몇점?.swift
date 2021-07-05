4611f72cf325e8760ec470e71a5b7f71a9671384
mandos1995 <mandos19950620@gmail.com>
2021-07-05 17:59:03 +0900

    BOJ 2755 sol

---
+++ BOJ/2755_이번학기 평점은 몇점?.swift
@@ -0,0 +1,14 @@
+import Foundation
+let score = ["A+": 4.3, "A0": 4.0, "A-": 3.7, "B+": 3.3, "B0": 3.0, "B-": 2.7,"C+": 2.3, "C0": 2.0, "C-": 1.7, "D+": 1.3, "D0": 1.0, "D-": 0.7, "F": 0.0]
+
+let n = Int(readLine()!)!
+var sum = 0.0
+var gradeSum = 0.0
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ")
+    let num = Double(input[1])!
+    let grade = input[2]
+    sum += num * score["\(grade)"]!
+    gradeSum += num
+}
+print(String(format: "%.2f", round(sum / gradeSum * 100) / 100))
