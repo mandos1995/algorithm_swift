11d169f3ba3c137c7770eec291a21aeda4cdce0f
mandos1995 <mandos19950620@gmail.com>
2021-07-05 17:44:57 +0900

    BOJ 2754 sol

--- BOJ/2754_학점계산.swift
+++ BOJ/2754_학점계산.swift
@@ -1,8 +1,10 @@
-//
-//  2754_학점계산.swift
-//  algorithm_swift
-//
-//  Created by 김민석 on 2021/07/05.
-//
-
 import Foundation
+let input = readLine()!
+let c_score = [4.3, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0.0]
+let score = ["A+", "A0", "A-","B+", "B0", "B-","C+", "C0","C-","D+","D0","D-","F"]
+
+for i in 0..<13 {
+    if input == score[i]{
+        print(c_score[i])
+    }
+}
