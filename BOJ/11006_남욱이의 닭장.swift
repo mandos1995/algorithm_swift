12d9836cb354fd443ba3aaacab4029116a2d3a86
mandos1995 <mandos19950620@gmail.com>
2021-07-27 23:34:07 +0900

    BOJ 11006 sol

---
+++ BOJ/11006_남욱이의 닭장.swift
@@ -0,0 +1,6 @@
+import Foundation
+let t = Int(readLine()!)!
+for _ in 0..<t {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    print(input[1] * 2 - input[0], input[1] - (input[1] * 2 - input[0]))
+}
