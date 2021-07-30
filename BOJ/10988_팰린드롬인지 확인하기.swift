4d0a6b708ded1edc4337cc03bc49539a7da21c43
mandos1995 <mandos19950620@gmail.com>
2021-07-30 14:00:39 +0900

    BOJ 10988 sol

--- BOJ/10988_팰린드롬인지 확인하기.swift
+++ BOJ/10988_팰린드롬인지 확인하기.swift
@@ -1,8 +1,2 @@
-//
-//  10988_팰린드롬인지 확인하기.swift
-//  algorithm_swift
-//
-//  Created by 김민석 on 2021/07/30.
-//
-
-import Foundation
+let str = readLine()!
+(str == String(str.reversed())) ? print(1) : print(0)
