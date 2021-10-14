ae8edbc05936bcf0924f4d3f8799e3bbce0afaae
mandos1995 <mandos19950620@gmail.com>
2021-10-15 08:27:03 +0900

    BOJ 2702 sol

---
+++ BOJ/2702_초6 수학.swift
@@ -0,0 +1,20 @@
+func gcd(_ n: Int, _ m: Int) -> Int {
+    if m == 0 {
+        return n
+    } else {
+        return gcd(m, n % m)
+    }
+}
+
+func lcm(_ n: Int, _ m: Int) -> Int {
+    return n * m / gcd(n, m)
+}
+
+let t = Int(readLine()!)!
+
+for _ in 0..<t {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let n = input[0], m = input[1]
+    print(lcm(n, m), gcd(n, m))
+}
+
