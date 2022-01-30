acd6b0c9507557644f13e389e555b1dc427e81c2
mandos1995 <mandos19950620@gmail.com>
2022-01-30 15:25:44 +0900

    BOJ 1946 sol

---
+++ BOJ/1946_신입 사원.swift
@@ -0,0 +1,77 @@
+import Foundation
+
+// 라이노님의 FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+// 풀이
+let file = FileIO()
+
+let t = file.readInt()
+
+for _ in 0..<t {
+    let n = file.readInt()
+    var mans: [[Int]] = []
+    for _ in 0..<n {
+        let input = [file.readInt(), file.readInt()]
+        mans.append(input)
+    }
+    mans.sort { $0[0] < $1[0] }
+    var count = 1
+    var rank = mans[0][1]
+    for i in 1..<n {
+        if mans[i][1] < rank {
+            count += 1
+            rank = mans[i][1]
+        }
+        if rank == 1 {
+            break
+        }
+    }
+    print(count)
+}
