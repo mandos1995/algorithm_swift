51bdb6e301fa60c1b26ca69863a22f4c9c66f4f0
mandos1995 <mandos19950620@gmail.com>
2022-01-31 00:16:31 +0900

    왕실의 나이트.swift 수정

--- 이것이 코딩테스트다/ch04_구현/왕실의 나이트.swift
+++ 이것이 코딩테스트다/ch04_구현/왕실의 나이트.swift
@@ -1,22 +1,22 @@
 // 현재 나이트의 위치 입력받기
-let inputData = readLine()!
-let endIdx = inputData.index(inputData.endIndex, offsetBy: -1)
-let row = Int(String(inputData[endIdx]))!
-let column = Int(inputData[inputData.startIndex].asciiValue!) - Int(UnicodeScalar("a").value) + 1
+let inputData = readLine()!.map { $0 }
+let row = Int(String(inputData[1]))!
+let column = Int(inputData[0].asciiValue! - Character("a").asciiValue! + 1)
 
 // 나이트가 이동할 수 있는 8가지 방향 정의
-let steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+let steps = [(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
 
 // 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
 var result = 0
 for step in steps {
     // 이동하고자 하는 위치 확인
-    let next_row = row + step.0
-    let next_column = column + step.1
+    let nextRow = row + step.0
+    let nextColumn = column + step.1
+    
     // 해당 위치로 이동이 가능하다면 카운트 증가
-    if next_row >= 1 && next_row <= 8 && next_column >= 1 && next_column <= 8 {
+    if nextRow >= 1 && nextRow < 8 && nextColumn >= 1 && nextColumn < 8 {
         result += 1
     }
-    
 }
+
 print(result)
