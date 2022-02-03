f6b1ee43022aa8d3c6dae3121773342904a7ed98
mandos1995 <mandos19950620@gmail.com>
2022-02-03 18:07:32 +0900

    미로 탈출.swift 수정

--- 이것이 코딩테스트다/ch05_DFS,BFS/미로 탈출.swift
+++ 이것이 코딩테스트다/ch05_DFS,BFS/미로 탈출.swift
@@ -1,21 +1,23 @@
+// n, m을 공백으로 구분하여 입력받기
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let n = input[0], m = input[1]
 
-var graph = [[Int]](repeating: [Int](repeating: 0, count: 0), count: n)
-
-for i in 0..<n {
-    let input = readLine()!
-    for j in input {
-        graph[i].append(Int(String(j))!)
-    }
+// 2차원 리스트의 맵 정보 입력받기
+var graph: [[Int]] = []
+for _ in 0..<n {
+    graph.append(readLine()!.map { Int(String($0))! })
 }
 
-// 이동할 네 방향 정의(상, 하, 좌, 우)
+// 이동할 네 방향 정의 (상, 하, 좌, 우)
 let dx = [-1, 1, 0, 0]
 let dy = [0, 0, -1, 1]
 
-func bfs(_ x: Int, _ y: Int) -> Int {
-    var queue = [(x, y)]
+// BFS 소스코드 구현
+func bfs(x: Int, y: Int) -> Int {
+    // Queue
+    var queue: [(Int, Int)] = []
+    queue.append((x, y))
+    
     // 큐가 빌 때까지 반복
     while !queue.isEmpty {
         let (x, y) = queue.removeFirst()
@@ -32,14 +34,14 @@ func bfs(_ x: Int, _ y: Int) -> Int {
                 continue
             }
             // 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-            if graph[nx][ny] == 1 {
+            if graph[nx][ny] == 1 && !(nx == 0 && ny == 0) {
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
             }
         }
     }
     // 가장 오른쪽 아래까지의 최단 거리 반환
-    return graph[n - 1][m - 1]
+    return graph[n - 1][m - 1]ç
 }
-// bfs를 수행한 결과 출력
-print(bfs(0, 0))
+// BFS를 수행한 결과 출력
+print(bfs(x: 0, y: 0))
