59d601c22a6963e89ea7c518945dd8eaa270f1d8
mandos1995 <mandos19950620@gmail.com>
2022-02-13 15:08:17 +0900

    BOJ 23808 sol

---
+++ BOJ/23808_골뱅이 찍기 - ㅂ.swift
@@ -0,0 +1,20 @@
+let n = Int(readLine()!)!
+
+for _ in 0..<2 * n {
+    print(String(repeating: "@", count: n),terminator: "")
+    print(String(repeating: " ", count: n * 3), terminator: "")
+    print(String(repeating: "@", count: n))
+}
+
+for _ in 0..<n {
+    print(String(repeating: "@", count: n * 5))
+}
+for _ in 0..<n {
+    print(String(repeating: "@", count: n),terminator: "")
+    print(String(repeating: " ", count: 3 * n),terminator: "")
+    print(String(repeating: "@", count: n))
+    
+}
+for _ in 0..<n {
+    print(String(repeating: "@", count: n * 5))
+}
