6fa4ec6558ee5199a0210b25a04f81860477485a
mandos1995 <mandos19950620@gmail.com>
2022-02-16 15:42:57 +0900

    BOJ 1715 sol

---
+++ BOJ/1715_카드 정렬하기.swift
@@ -0,0 +1,73 @@
+public struct Heap<T> {
+    var nodes: [T] = []
+    let comparer: (T,T) -> Bool
+    var isEmpty: Bool {
+        return nodes.isEmpty
+    }
+    var count: Int {
+        return nodes.count
+    }
+    init(comparer: @escaping (T,T) -> Bool) {
+        self.comparer = comparer
+    }
+    func peek() -> T? {
+        return nodes.first
+    }
+    mutating func insert(_ element: T) {
+        var index = nodes.count
+        nodes.append(element)
+        while index > 0, !comparer(nodes[index],nodes[(index-1)/2]) {
+            nodes.swapAt(index, (index-1)/2)
+            index = (index-1)/2 }
+    }
+    mutating func delete() -> T? {
+        guard !nodes.isEmpty else { return nil }
+        if nodes.count == 1 {
+            return nodes.removeFirst()
+        }
+        let result = nodes.first
+        nodes.swapAt(0, nodes.count-1)
+        _ = nodes.popLast()
+        var index = 0
+        while index < nodes.count {
+            let left = index * 2 + 1
+            let right = left + 1
+            if right < nodes.count {
+                if comparer(nodes[left], nodes[right]), !comparer(nodes[right], nodes[index]) {
+                    nodes.swapAt(right, index)
+                    index = right
+                } else if !comparer(nodes[left], nodes[index]){
+                    nodes.swapAt(left, index)
+                    index = left
+                } else {
+                    break
+                }
+            } else if left < nodes.count {
+                if !comparer(nodes[left], nodes[index]) {
+                    nodes.swapAt(left, index)
+                    index = left
+                } else { break }
+                
+            } else { break }
+        }
+        return result
+    }
+}
+extension Heap where T: Comparable {
+    init() {
+        self.init(comparer: >) }
+}
+
+let n = Int(readLine()!)!
+var heap = Heap<Int>()
+for _ in 0..<n {
+    heap.insert(Int(readLine()!)!)
+}
+
+var result = 0
+while heap.count > 1 {
+    let sum = heap.delete()! + heap.delete()!
+    result += sum
+    heap.insert(sum)
+}
+print(result)
