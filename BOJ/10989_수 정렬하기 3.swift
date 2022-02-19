45e47c6f73ed00014a45aaceb2421de25bccb8a8
mandos1995 <mandos19950620@gmail.com>
2022-02-19 15:09:36 +0900

    BOJ 10989 sol

---
+++ BOJ/10989_수 정렬하기 3.swift
@@ -0,0 +1,70 @@
+import Foundation
+
+// 라이노님의 FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+let file = FileIO()
+var nums: [Int] = []
+var countingNum = [Int](repeating: 0, count: 10001)
+for _ in 0..<file.readInt() {
+    nums.append(file.readInt())
+}
+for num in nums {
+    countingNum[num] += 1
+}
+var output = ""
+for i in 1...10000 {
+    if countingNum[i] != 0 {
+        for _ in 0..<countingNum[i] {
+            output += "\(i)\n"
+        }
+    }
+}
+output.removeLast()
+print(output)
