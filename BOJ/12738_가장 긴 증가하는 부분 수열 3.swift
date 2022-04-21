80fc23f41699134837a87097448cc311dbbb81c8
mandos1995 <mandos19950620@gmail.com>
2022-04-21 17:23:01 +0900

    BOJ 12738 sol

---
+++ BOJ/12738_가장 긴 증가하는 부분 수열 3.swift
@@ -0,0 +1,26 @@
+let n = Int(readLine()!)!
+let array = readLine()!.split(separator: " ").map { Int($0)! }
+var cache = [-1_000_000_001]
+
+for arr in array {
+    if cache.last! < arr {
+        cache.append(arr)
+        continue
+    }
+    
+    var start = 0
+    var end = cache.count
+    
+    while start < end {
+        let mid = (start + end) / 2
+        
+        if cache[mid] < arr {
+            start = mid + 1
+        } else {
+            end = mid
+        }
+    }
+    cache[end] = arr
+}
+
+print(cache.count - 1)
