a87be7c581d5d5ee7145a575ee06fbd38395f78a
mandos1995 <mandos19950620@gmail.com>
2022-05-22 16:02:31 +0900

    BOJ 21736 sol

---
+++ BOJ/21736_헌내기는 친구가 필요해.swift
@@ -0,0 +1,53 @@
+func isVaildCoord(y: Int, x: Int) -> Bool {
+    return 0..<n ~= y && 0..<m ~= x
+}
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input[0], m = input[1]
+var graph: [[String]] = []
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
+for _ in 0..<n {
+    graph.append(readLine()!.map { String($0) })
+}
+
+var start = (0, 0)
+for y in 0..<n {
+    for x in 0..<m {
+        if graph[y][x] == "I" {
+            start = (y, x)
+        }
+    }
+}
+
+let dx = [1, 0, -1, 0]
+let dy = [0, 1, 0, -1]
+
+var answer = 0
+func bfs(y: Int, x: Int) {
+    var queue = [(y, x)]
+    var index = 0
+    while index < queue.count {
+        let y = queue[index].0
+        let x = queue[index].1
+        visited[y][x] = true
+        
+        if graph[y][x] == "P" {
+            answer += 1
+        }
+        
+        for i in 0..<4 {
+            let ny = y + dy[i]
+            let nx = x + dx[i]
+            
+            if isVaildCoord(y: ny, x: nx) && !visited[ny][nx] && graph[ny][nx] != "X" {
+                visited[ny][nx] = true
+                queue.append((ny, nx))
+            }
+        }
+        index += 1
+    }
+}
+
+bfs(y: start.0, x: start.1)
+
+answer == 0 ? print("TT") : print(answer)
