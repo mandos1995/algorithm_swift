cd49e1f2878b28588c1a6accef3436605bbca1b7
mandos1995 <mandos19950620@gmail.com>
2022-05-24 18:30:45 +0900

    BOJ 10814 sol

---
+++ BOJ/10814_나이순 정렬.swift
@@ -0,0 +1,70 @@
+import Foundation
+
+// 라이노님의 FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+// 풀이
+let file = FileIO()
+struct User {
+    let age: Int
+    let name: String
+    let index: Int
+}
+let n = file.readInt()
+var users: [User] = []
+for index in 0..<n {
+    let age = file.readInt(), name = file.readString()
+    users.append(User(age: age, name: name, index: index))
+}
+
+users.sort { $0.age == $1.age ? $0.index < $1.index : $0.age < $1.age }
+
+for user in users {
+    print(user.age, user.name)
+}
