171791ef91c4cef4a15816fcbb8a82534c9cec86
mandos1995 <mandos19950620@gmail.com>
2022-05-24 18:21:15 +0900

    BOJ 11650 sol

---
+++ BOJ/11650_좌표 정렬하기.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+// 라이노님의 FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+// 풀이
+let file = FileIO()
+struct Coord {
+    let x: Int
+    let y: Int
+}
+let n = file.readInt()
+var coords: [Coord] = []
+for _ in 0..<n {
+    let x = file.readInt(), y = file.readInt()
+    coords.append(Coord(x: x, y: y))
+}
+
+coords.sort { $0.x == $1.x ? $0.y < $1.y : $0.x < $1.x }
+
+for coord in coords {
+    print(coord.x, coord.y)
+}
