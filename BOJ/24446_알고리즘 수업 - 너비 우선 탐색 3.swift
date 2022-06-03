87d03ff123645ab32bf52e8ab9c0dbf6c64c80f0
mandos1995 <mandos19950620@gmail.com>
2022-06-03 15:06:57 +0900

    BOJ 24446 sol

---
+++ BOJ/24446_알고리즘 수업 - 너비 우선 탐색 3.swift
@@ -0,0 +1,35 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input[0], m = input[1], r = input[2]
+var graph = [[Int]](repeating: [Int](repeating: 0, count: 0), count: n + 1)
+for _ in 0..<m {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let a = input[0], b = input[1]
+    graph[a].append(b)
+    graph[b].append(a)
+}
+graph = graph.map { $0.sorted(by: > )}
+var visited = [Int](repeating: -1, count: n + 1)
+
+func bfs(start: Int) {
+    visited[start] = 0
+    var queue = [(start, 0)]
+    var index = 0
+    var count = 2
+    while queue.count > index {
+        let startNode = queue[index].0
+        let d = queue[index].1
+        for nextNode in graph[startNode] {
+            if visited[nextNode] == -1 {
+                visited[nextNode] = d + 1
+                count += 1
+                queue.append((nextNode, d + 1))
+            }
+        }
+        index += 1
+    }
+}
+bfs(start: r)
+
+for v in visited.dropFirst() {
+    print(v)
+}
