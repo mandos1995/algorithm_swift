e3cdf30aee1ca678b117091bc338ab39c2c398a1
mandos1995 <mandos19950620@gmail.com>
2022-08-16 11:08:17 +0900

    BOJ 1325 sol

---
+++ BOJ/1325_효율적인 해킹.swift
@@ -0,0 +1,96 @@
+import Foundation
+
+// 라이노님의 FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+// 풀이
+let file = FileIO()
+
+let n = file.readInt(), m = file.readInt()
+var graph = [[Int]](repeating: [Int](repeating: 0, count: 0), count: n + 1)
+var visited = [Bool](repeating: false, count: n + 1)
+
+for _ in 0..<m {
+    let a = file.readInt(), b = file.readInt()
+    graph[b].append(a)
+}
+
+func bfs(start: Int) -> Int {
+    visited = [Bool](repeating: false, count: n + 1)
+    var queue = [start]
+    var index = 0
+    var count = 0
+    visited[start] = true
+    while queue.count > index {
+        let startNode = queue[index]
+        count += 1
+        for nextNode in graph[startNode] {
+            if !visited[nextNode] {
+                visited[nextNode] = true
+                queue.append(nextNode)
+            }
+        }
+        index += 1
+    }
+    return count
+}
+
+var answer = [Int](repeating: 0, count: n + 1)
+
+for i in 1...n {
+     answer[i] = bfs(start: i)
+}
+
+let maxCount = answer.max()!
+
+for i in 1...n {
+    if answer[i] == maxCount {
+        print(i, terminator: " ")
+    }
+}
