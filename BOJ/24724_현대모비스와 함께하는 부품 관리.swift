facf306edb99058eb7abf6f7b18b1347f2cddc8c
mandos1995 <mandos19950620@gmail.com>
2022-09-04 16:29:31 +0900

    BOJ 24724 sol

---
+++ BOJ/24724_현대모비스와 함께하는 부품 관리.swift
@@ -0,0 +1,10 @@
+let t = Int(readLine()!)!
+for i in 1...t {
+    let n = Int(readLine()!)!
+    _ = readLine()!
+    for _ in 0..<n {
+        _ = readLine()!
+    }
+    print("Material Management \(i)")
+    print("Classification ---- End!")
+}
