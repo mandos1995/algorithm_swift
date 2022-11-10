4ddec6162f4d3e7b3571b4a264aac5d950c84a7c
mandos1995 <mandos19950620@gmail.com>
2022-11-10 20:46:11 +0900

    BOJ 10971 sol

---
+++ BOJ/10971_외판원 순회 2.swift
@@ -0,0 +1,34 @@
+let n = Int(readLine()!)!
+var graph: [[Int]] = []
+let INF = 1_234_567_890
+for _ in 0..<n {
+    graph.append(readLine()!.split(separator: " ").map { Int(String($0))! })
+}
+
+var cache = [[Int]](repeating: [Int](repeating: INF, count: (1 << n)), count: n)
+
+func tsp(now: Int, visited: Int) -> Int {
+    if cache[now][visited] != INF { return cache[now][visited] }
+    
+    if visited == (1 << n) - 1 {
+        if graph[now][0] == 0 {
+            return INF
+        } else {
+            return graph[now][0]
+        }
+    }
+
+    var answer = INF
+    for i in 1..<n {
+        if graph[now][i] == 0 || visited & (1 << i) != 0 {
+            continue
+        }
+        answer = min(graph[now][i] + tsp(now: i, visited: visited | (1 << i)), answer)
+    }
+    
+    cache[now][visited] = answer
+    
+    return answer
+}
+
+print(tsp(now: 0, visited: 1))
