53083b073900b8b0100569b4512d97fd09e927e2
mandos1995 <mandos19950620@gmail.com>
2022-11-20 22:55:10 +0900

    ✨ BOJ 25372 sol

--- BOJ/25372_성택이의 은밀한 비밀번호.swift
+++ BOJ/25372_성택이의 은밀한 비밀번호.swift
@@ -1,9 +1,9 @@
 let n = Int(readLine()!)!
 for _ in 0..<n {
-let word = readLine()!
-if word.count > 5 && word.count < 10 {
-print("yes")
-} else {
-print("no")
-}
+    let word = readLine()!
+    if word.count > 5 && word.count < 10 {
+        print("yes")
+    } else {
+        print("no")
+    }
 }
