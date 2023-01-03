a045eebbf35ba66d2294016290de6a68429aa236
mandos1995 <mandos19950620@gmail.com>
2023-01-03 10:25:16 +0900

    숨어있는 숫자의 덧셈 (2) sol

---
+++ Programmers/Level 0/숨어있는 숫자의 덧셈 (2).swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func solution(_ my_string:String) -> Int {
+    return my_string.getArrayAfterRegex(regex: "[0-9]+").compactMap { Int($0) }.reduce(0, +)
+}
+
+extension String{
+    func getArrayAfterRegex(regex: String) -> [String] {
+        
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: self,
+                                        range: NSRange(self.startIndex..., in: self))
+            return results.map {
+                String(self[Range($0.range, in: self)!])
+            }
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+}
