d610b84b5c227bd67fd8df7ae3e3272af6b279d2
mandos1995 <mandos19950620@gmail.com>
2023-05-25 11:11:34 +0900

    BOJ 17387 update

--- BOJ/17000 ~ 17999/17387_선분 교차 2.swift
+++ BOJ/17000 ~ 17999/17387_선분 교차 2.swift
@@ -1,21 +1,25 @@
-func CCW(_ x1: Double, _ y1: Double, _ x2: Double, _ y2: Double, _ x3: Double, _ y3: Double) -> Int {
-    let result = (x1 * y2 + x2 * y3 + x3 * y1) - (y1 * x2 + y2 * x3 + y3 * x1)
+typealias Point = (x: Double, y: Double)
+
+func CCW(_ a: Point, _ b: Point, _ c: Point) -> Int {
+    let result = (a.x * b.y + b.x * c.y + c.x * a.y) - (a.y * b.x + b.y * c.x + c.y * a.x)
     return result == 0 ? 0 : result > 0 ? 1 : -1
 }
 
 let L1 = readLine()!.split(separator: " ").map { Double($0)! }
 let L2 = readLine()!.split(separator: " ").map { Double($0)! }
 
-let x = [L1[0], L1[2], L2[0], L2[2]]
-let y = [L1[1], L1[3], L2[1], L2[3]]
+var a = Point(x: L1[0], y: L1[1])
+var b = Point(x: L1[2], y: L1[3])
+var c = Point(x: L2[0], y: L2[1])
+var d = Point(x: L2[2], y: L2[3])
 
-let ccwValue = [CCW(x[0], y[0], x[1], y[1], x[2], y[2]) * CCW(x[0], y[0], x[1], y[1], x[3], y[3]),
-                CCW(x[2], y[2], x[3], y[3], x[0], y[0]) * CCW(x[2], y[2], x[3], y[3], x[1], y[1])]
+let ccwValue = [CCW(a, b, c) * CCW(a, b, d), CCW(c, d, a) * CCW(c, d, b)]
 
 if ccwValue[0] == 0 && ccwValue[1] == 0 {
-    let x1 = min(x[0], x[1]), y1 = min(y[0], y[1]), x2 = max(x[0], x[1]), y2 = max(y[0], y[1])
-    let x3 = min(x[2], x[3]), y3 = min(y[2], y[3]), x4 = max(x[2], x[3]), y4 = max(y[2], y[3])
-    if x1 <= x4 && x3 <= x2 && y1 <= y4 && y3 <= y2 {
+    if a > b { swap(&a, &b) }
+    if c > d { swap(&c, &d) }
+    
+    if a <= d && b >= c {
         print(1)
     } else {
         print(0)
