9858444bac97ad4a5b12812710c64fae2d0ef439
mandos1995 <mandos19950620@gmail.com>
2023-06-06 15:25:22 +0900

    BOJ 27889 sol

---
+++ BOJ/27000 ~ 27999/27889_특별한 학교 이름.swift
@@ -0,0 +1,5 @@
+let dict = ["NLCS":"North London Collegiate School",
+             "BHA":"Branksome Hall Asia",
+             "KIS":"Korea International School",
+             "SJA":"St. Johnsbury Academy"]
+print(dict[readLine()!]!)
