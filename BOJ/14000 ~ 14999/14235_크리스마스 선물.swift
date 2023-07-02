16fbd08adcb53763e3a125e853e564a2725bb581
mandos1995 <mandos19950620@gmail.com>
2023-07-02 13:37:43 +0900

    BOJ 14235 sol

---
+++ BOJ/14000 ~ 14999/14235_크리스마스 선물.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  algorithm_swift
+//
+//  Created by 김민석 on 2023/07/02.
+//
+
+import Foundation
---
+++ BOJ/14000 ~ 14999/14235_크리스마스 선물.swift
@@ -0,0 +1,76 @@
+struct Heap<T: Comparable> {
+    private var elements: [T] = []
+    private let comparer: (T, T) -> Bool
+    
+    var isEmpty: Bool {
+        return elements.count <= 1
+    }
+    
+    var top: T? {
+        return isEmpty ? nil : elements[1]
+    }
+    
+    init(comparer: @escaping (T,T) -> Bool) {
+        self.comparer = comparer
+    }
+    
+    mutating func insert(element: T) {
+        if elements.isEmpty {
+            elements.append(element)
+            elements.append(element)
+            return
+        }
+        elements.append(element)
+        swimUp(index: elements.count - 1)
+    }
+    
+    mutating private func swimUp(index: Int) {
+        var index = index
+        while index > 1 && comparer(elements[index], elements[index / 2]) {
+            elements.swapAt(index, index / 2)
+            index /= 2
+        }
+    }
+    
+    mutating func pop() -> T? {
+        if elements.count < 2 { return nil }
+        elements.swapAt(1, elements.count - 1)
+        let deletedElement = elements.removeLast()
+        diveDown(index: 1)
+        return deletedElement
+    }
+    
+    mutating private func diveDown(index: Int) {
+        var swapIndex = index
+        var isSwap = false
+        let leftIndex = index * 2
+        let rightIndex = index * 2 + 1
+
+        if leftIndex < elements.endIndex && comparer(elements[leftIndex], elements[swapIndex]) {
+            swapIndex = leftIndex
+            isSwap = true
+        }
+        
+        if rightIndex < elements.endIndex && comparer(elements[rightIndex], elements[swapIndex]) {
+            swapIndex = rightIndex
+            isSwap = true
+        }
+
+        if isSwap {
+            elements.swapAt(swapIndex, index)
+            diveDown(index: swapIndex)
+        }
+    }
+}
+
+var maxHeap = Heap<Int>(comparer: >)
+
+let n = Int(readLine()!)!
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    if input[0] == 0 {
+        print(maxHeap.pop() ?? -1)
+        continue
+    }
+    input[1...].forEach { maxHeap.insert(element: $0) }
+}
