fb8d694e9310ce61a91b669d66035ac3fd1ecba6
mandos1995 <mandos19950620@gmail.com>
2023-09-29 17:27:32 +0900

    BOJ 28290 sol

---
+++ BOJ/28000 ~ 28999/28290_안밖? 밖안? 계단? 역계단?.swift
@@ -0,0 +1,12 @@
+switch readLine()! {
+case "fdsajkl;", "jkl;fdsa":
+    print("in-out")
+case "asdf;lkj", ";lkjasdf":
+    print("out-in")
+case "asdfjkl;":
+    print("stairs")
+case ";lkjfdsa":
+    print("reverse")
+default:
+    print("molu")
+}
