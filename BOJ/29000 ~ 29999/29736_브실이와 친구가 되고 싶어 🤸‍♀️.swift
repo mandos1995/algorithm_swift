95bd327b9da38c8ab125b646a7a8ec39bea68523
mandos1995 <mandos19950620@gmail.com>
2023-10-21 17:05:10 +0900

    BOJ 29736 sol

--- BOJ/29000 ~ 29999/29736_브실이와 친구가 되고 싶어 🤸‍♀️.swift
+++ BOJ/29000 ~ 29999/29736_브실이와 친구가 되고 싶어 🤸‍♀️.swift
@@ -1,8 +1,7 @@
-//
-//  29736.swift
-//  PS
-//
-//  Created by 김민석 on 10/21/23.
-//
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let a = input[0], b = input[1]
+let kx = readLine()!.split(separator: " ").map { Int($0)! }
+let k = kx[0], x = kx[1]
 
-import Foundation
+let friends = Set((a...b)).intersection(Set((k-x)...(k+x))).count
+print(friends == 0 ? "IMPOSSIBLE" : friends)
