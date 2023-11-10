8f6ba90847e81b18113d011e174b326dc19ea7dc
minseok kim <77199797+mandos1995@users.noreply.github.com>
2023-11-10 13:08:54 +0900

    BOJ 28691 sol

---
+++ BOJ/28000 ~ 28999/28691_정보보호학부 동아리 소개.swift
@@ -0,0 +1,8 @@
+let s = readLine()!
+switch s {
+    case "M": print("MatKor")
+    case "W": print("WiCys")
+    case "C": print("CyKor")
+    case "A": print("AlKor")
+    default: print("$clear")
+}
